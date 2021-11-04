4ec530a981eb6352a81b3ba980e595c6cddf5389
unknown <gumiakk1234@gmail.com>
2021-11-04 18:14:33 +0100

    fix of products position

--- php/getProducts.php
+++ php/getProducts.php
@@ -1,4 +1,5 @@
 <div class="produkty">
+<ul id="produkt-grid" class="newproduct_grid product_list grid row gridcount">
 <?php
 include('load_database.php') ;
 
@@ -8,22 +9,21 @@ $stmt = $pdo->query('SELECT * FROM products');
 $html = '';
 
 foreach ($stmt as $row){
-    $html .= '<div><form method="post" action="produkt.php">';
     $stmt2 = $pdo->query('SELECT * FROM gallery WHERE main = 1 AND id_product='.$row['id_product']);
-    $html.= '<ul id="produkt-grid" class="newproduct_grid product_list grid row gridcount">';
     $html.= '<li class="col-xs-12 col-sm-6 col-md-6 col-lg-4" id="produkt_calosc">';
+    $html .= '<form method="post" action="produkt.php">';
     foreach($stmt2 as $row2){
         $html .='<input type="image" src="'.$row2['foto'].'" alt="Submit" class="produkt"><input type="hidden" name="id_product" value="'.$row['id_product'].'">';
     }
     $html .='<div class="product-description">';
-    $html .='<span class="h3 product-title"><a href="produkt.php" class="produkt1">'.$row['product_name'].'</a></span>';
+    $html .='<span class="product-title"><a href="produkt.php" class="produkt1">'.$row['product_name'].'</a></span>';
     $html .='<div class="product-price">';
-    $html .='<span class="cena">'. $row['price'].'</span>';
-    $html .='</div></div></li></ul>';
-    $html .='</div></form>';
+    $html .='<span class="cena">'. $row['price'].' zł</span>';
+    $html .='</div></div></form></li>';
 }
 echo $html;
 ?>
+</ul>
 </div>
 
 
